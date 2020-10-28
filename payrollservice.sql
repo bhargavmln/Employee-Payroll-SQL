b2fcfe2aaab5b784c99d6c90d4bf69e96b3f01c9
bhargavmln <bhargavmln@gmail.com>
2020-10-28 17:59:30 +0530

    [bhargavmln]create: created employee_payroll table

--- payrollservice.sql
+++ payrollservice.sql
@@ -4,3 +4,15 @@ CREATE DATABASE payroll_service; #Created database
 SHOW databases;
 USE payroll_service;
 SELECT database();
+
+#UC2
+CREATE TABLE employee_payroll
+(
+id       INT unsigned NOT NULL auto_increment,		#ID is set on auto increment
+name     VARCHAR(150) NOT NULL,
+salary   DOUBLE NOT NULL,
+start    DATE NOT NULL,
+PRIMARY KEY (id)  									#Set ID as primary key
+);
+DESCRIBE employee_payroll;
+
